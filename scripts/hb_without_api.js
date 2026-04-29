90b952436fc93321897a2010833d97c87c561123
killer whale <thekietvn4949@gmail.com>
2026-04-29 23:05:50 +0300

    last cc

--- scripts/hb_without_api.js
+++ scripts/hb_without_api.js
@@ -1,5 +1,5 @@
 const API_URL = "http://127.0.0.1:8080";
-const API_KEY = "48b2c8292119154f909f348508a9eb1d69e44c175ced1788e4fd78e722a176b7";
+
 const firstName = document.getElementById("first_name");
 const lastName = document.getElementById("last_name");
 const roomSelect = document.getElementById("room_number");
@@ -8,36 +8,41 @@ const dateTo = document.getElementById("date_to");
 const guestRequest = document.getElementById("guest_request");
 const okBtn = document.getElementById("ok-btn");
 const bookingList = document.getElementById("booking_list");
+
 export async function loadRoomNumber() {
     const res = await fetch(`${API_URL}/hotel`);
     const rooms = await res.json();
+
     roomSelect.innerHTML = "";
+
     rooms.forEach(room => {
         const option = document.createElement("option");
         option.value = room.id;
         option.textContent = `Room ${room.room_number}`;
         roomSelect.appendChild(option);
     });
-};
+}
+
 window.saveStars = async function(booking_id, stars) {
     console.log(`booking ${booking_id} gets ${stars} stars`);
+
     await fetch(`${API_URL}/bookings/${booking_id}`, {
         method: "PUT",
         headers: {
-            "Content-Type":"application/json",
-            "X-API-Key":API_KEY
+            "Content-Type": "application/json"
         },
-        body:JSON.stringify({
-            stars:parseInt(stars)
+        body: JSON.stringify({
+            stars: parseInt(stars)
         })
     });
 };
+
 export async function loadBookingList() {
-    const res = await fetch(`${API_URL}/bookings`, {
-        headers: {'X-API-Key': API_KEY}
-    });
+    const res = await fetch(`${API_URL}/bookings`);
     const bookings = await res.json();
+
     bookingList.innerHTML = "";
+
     bookings.forEach(booking => {
         bookingList.innerHTML += `
             <li class="mb-2">
@@ -56,10 +61,12 @@ export async function loadBookingList() {
             </li>
         `;
     });
-};
+}
+
 export async function booking() {
-    if(!firstName.value || !lastName.value) return;
-    if((!dateFrom.value || !dateTo.value) || (dateFrom.value > dateTo.value)) return;
+    if (!firstName.value || !lastName.value) return;
+    if (!dateFrom.value || !dateTo.value || dateFrom.value > dateTo.value) return;
+
     const guestRes = await fetch(`${API_URL}/guests`, {
         method: "POST",
         headers: {
@@ -70,34 +77,24 @@ export async function booking() {
             last_name: lastName.value
         })
     });
-    //const guestData = await guestRes.json();
-    //const guestId = guestData.id;
+
+    const guestData = await guestRes.json();
+
     const bookingRes = await fetch(`${API_URL}/bookings`, {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json', 
-            'X-API-Key': API_KEY
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             room_id: parseInt(roomSelect.value),
-            //guest_id: guestId,
+            guest_id: guestData.id,
             date_from: dateFrom.value,
             date_to: dateTo.value,
             other_info: guestRequest.value
         })
     });
-    if(bookingRes.ok) loadBookingList();
-};
-/* const guestSelect = document.getElementById("guest_select");
-export async function loadGuest() {
-    const res = await fetch(`${API_URL}/guests`);
-    const guests = await res.json();
-    guestSelect.innerHTML = "";
-    guests.forEach(guest => {
-        const guestOption = document.createElement("option");
-        guestOption.value = guest.id;
-        guestOption.textContent = `${guest.first_name} ${guest.last_name}`
-        guestSelect.append(guestOption);
-    });
-}; */
+
+    if (bookingRes.ok) loadBookingList();
+}
+
 okBtn.addEventListener("click", booking);
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 //import {IpAPI, HotelAPI} from "./api.js";
-import {loadRoomNumber, loadBookingList} from "./hotel_booking.js";
+//import {loadRoomNumber, loadBookingList} from "./hotel_booking.js";
 //import {loadGuest} from "./guest.js";
 //IpAPI();
 //HotelAPI();
+//loadRoomNumber();
+//loadBookingList();
+//loadGuest();
+
+import { loadRoomNumber, loadBookingList } from "./hb_without_api.js";
 loadRoomNumber();
 loadBookingList();
-//loadGuest();
